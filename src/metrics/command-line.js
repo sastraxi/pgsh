fce5f79132b9010cdf53e07b6463ab3430bfb4b3
Cameron Gorrie <sastraxi@gmail.com>
2019-11-28 22:49:21 -0500

    don't leak PII

--- src/metrics/command-line.js
+++ src/metrics/command-line.js
@@ -1,11 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const quickHash = require('../util/quick-hash');
+
 let commandLine = null;
 
-const quickHash = require('../util/quick-hash');
+const hidePath = (str) => {
+  if (fs.existsSync(str)) {
+    return path.basename(str);
+  }
+  return str;
+};
 
 module.exports = {
   set: (...sensitiveStrings) => {
-    commandLine = process.argv.map(x =>
-      (sensitiveStrings.indexOf(x) !== -1 ? quickHash(x) : x));
+    const args = process.argv;
+
+    if (args.length >= 2) {
+      args[0] = hidePath(args[0]);
+    }
+
+    if (args.length >= 3) {
+      args[1] = hidePath(args[1]);
+    }
+
+    commandLine = args.map(x =>
+      (sensitiveStrings.indexOf(x) !== -1
+        ? quickHash(x)
+        : x));
   },
   get: () => commandLine,
 };
