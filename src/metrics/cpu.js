052e3d6a70e4ae92467888ae5422fbddb90b78c4
Cameron Gorrie <sastraxi@gmail.com>
2019-11-28 21:20:40 -0500

    fix cpu  metrics

--- src/metrics/cpu.js
+++ src/metrics/cpu.js
@@ -3,12 +3,13 @@ const os = require('os');
 
 const averageOf = (cpus) => {
   const avg = (key, key2) =>
-    cpus.map(x => !(key2 ? x[key] : x[key][key2]))
-      .reduce((a, b) => (a + b));
+    cpus.map(x => (!key2 ? x[key] : x[key][key2]))
+      .reduce((a, b) => (a + b), 0) / cpus.length;
 
   if (cpus.length === 0) return undefined;
   return {
-    model: `${cpus.length}x ${cpus[0].model}`,
+    model: cpus[0].model,
+    cores: cpus.length,
     speed: avg('speed'),
     times: {
       user: avg('times', 'user'),
