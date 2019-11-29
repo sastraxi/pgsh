264e2be4d1fb3e31c700aecec0d5f1cc96b1d4d5
Cameron Gorrie <sastraxi@gmail.com>
2019-11-28 22:12:59 -0500

    document force_disable_telemetry

--- src/pgshrc/default.js
+++ src/pgshrc/default.js
@@ -26,5 +26,5 @@ module.exports = {
   template: 'template1', /* when creating databases */
   fallback_database: 'postgres', /* if connecting to the named database fails, for e.g. listing */
   dotenv_encoding: 'utf8', /* parse/encode .env in this encoding */
-  force_disable_telemetry: false,
+  force_disable_telemetry: false, /* override the global setting for this repo */
 };
