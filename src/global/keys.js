ddb36294a671bfdf1a9d8b33f07096b9f95961e6
Cameron Gorrie <sastraxi@gmail.com>
2019-11-30 23:31:12 -0500

    persist quick hash key in global

--- src/global/keys.js
+++ src/global/keys.js
@@ -1,9 +1,11 @@
 const LAST_SENT = 'last_sent';
 const METRICS_ENABLED = 'metrics_enabled';
 const METRICS_IN_PROGRESS = 'metrics_lock';
+const QUICK_HASH_KEY = 'quick_hash_key';
 
 module.exports = {
   LAST_SENT,
   METRICS_ENABLED,
   METRICS_IN_PROGRESS,
+  QUICK_HASH_KEY,
 };
--- src/util/quick-hash.js
+++ src/util/quick-hash.js
@@ -1,10 +1,18 @@
 const crypto = require('crypto');
 
-const buf = crypto.randomBytes(256);
+const randomString = require('./random-string');
+const global = require('../global');
+const { QUICK_HASH_KEY } = require('../global/keys');
+
+let key = global.get(QUICK_HASH_KEY);
+if (!key) {
+  key = randomString(48);
+  global.set(QUICK_HASH_KEY, key);
+}
 
 const quickHash = (str) => {
   const hexString = crypto
-    .createHmac('sha256', buf.toString('hex'))
+    .createHmac('sha256', key)
     .update(str)
     .digest('hex')
     .substr(0, 6);
